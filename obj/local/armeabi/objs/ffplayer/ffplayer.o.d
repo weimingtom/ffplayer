a90cc8767dfe9dfc168d2504c5c2a7c7908bd323
chengkaizone <chengkaizone@163.com>
2014-05-03 17:06:42 +0800

    初始导入 开源

---
+++ obj/local/armeabi/objs/ffplayer/ffplayer.o.d
@@ -0,0 +1,230 @@
+obj/local/armeabi/objs/ffplayer/ffplayer.o: jni/ffplayer.c \
+ jni/libavcodec/avcodec.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/errno.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/errno.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/errno.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/errno.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/errno-base.h \
+ jni/libavutil/samplefmt.h jni/libavutil/avutil.h jni/libavutil/common.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/ctype.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/inttypes.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdint.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/limits.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/limits.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/limits.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/internal_types.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/limits.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/syslimits.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/page.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/math.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdio.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/string.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/malloc.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/alloca.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/strings.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/memory.h \
+ jni/libavutil/attributes.h jni/libavutil/avconfig.h jni/libavutil/mem.h \
+ jni/libavutil/error.h jni/libavutil/version.h \
+ jni/libavutil/mathematics.h jni/libavutil/rational.h \
+ jni/libavutil/intfloat.h jni/libavutil/intfloat_readwrite.h \
+ jni/libavutil/log.h jni/libavutil/pixfmt.h jni/libavutil/avutil.h \
+ jni/libavutil/cpu.h jni/libavutil/dict.h jni/libavutil/log.h \
+ jni/libavutil/pixfmt.h jni/libavutil/rational.h \
+ jni/libavutil/audioconvert.h jni/libavcodec/version.h \
+ jni/libavformat/avformat.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/time.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/time.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/time.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/siginfo.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/siginfo.h \
+ jni/libavcodec/avcodec.h jni/libavformat/avio.h jni/libavutil/common.h \
+ jni/libavformat/version.h jni/libavutil/time.h jni/libswscale/swscale.h \
+ jni/libswscale/version.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/jni.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/pthread.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/signal.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/signal.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/signal.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/sigcontext.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sched.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/log.h
+
+jni/libavcodec/avcodec.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/errno.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/errno.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/errno.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/errno.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/errno-base.h:
+
+jni/libavutil/samplefmt.h:
+
+jni/libavutil/avutil.h:
+
+jni/libavutil/common.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/ctype.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/inttypes.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/limits.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/limits.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/limits.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/internal_types.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/limits.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/syslimits.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/page.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/math.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdio.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdlib.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/string.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/malloc.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/alloca.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/strings.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/memory.h:
+
+jni/libavutil/attributes.h:
+
+jni/libavutil/avconfig.h:
+
+jni/libavutil/mem.h:
+
+jni/libavutil/error.h:
+
+jni/libavutil/version.h:
+
+jni/libavutil/mathematics.h:
+
+jni/libavutil/rational.h:
+
+jni/libavutil/intfloat.h:
+
+jni/libavutil/intfloat_readwrite.h:
+
+jni/libavutil/log.h:
+
+jni/libavutil/pixfmt.h:
+
+jni/libavutil/avutil.h:
+
+jni/libavutil/cpu.h:
+
+jni/libavutil/dict.h:
+
+jni/libavutil/log.h:
+
+jni/libavutil/pixfmt.h:
+
+jni/libavutil/rational.h:
+
+jni/libavutil/audioconvert.h:
+
+jni/libavcodec/version.h:
+
+jni/libavformat/avformat.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/time.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/time.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/time.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/siginfo.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/siginfo.h:
+
+jni/libavcodec/avcodec.h:
+
+jni/libavformat/avio.h:
+
+jni/libavutil/common.h:
+
+jni/libavformat/version.h:
+
+jni/libavutil/time.h:
+
+jni/libswscale/swscale.h:
+
+jni/libswscale/version.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/jni.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/pthread.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/signal.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/signal.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/signal.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/sigcontext.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sched.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/log.h:
---
+++ obj/local/armeabi/objs/pack/pack.o.d
@@ -0,0 +1,220 @@
+obj/local/armeabi/objs/pack/pack.o: jni/pack.c \
+ jni/ffmpeg/libavcodec/avcodec.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/errno.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/errno.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/errno.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/errno.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/errno-base.h \
+ jni/ffmpeg/libavutil/samplefmt.h jni/ffmpeg/libavutil/avutil.h \
+ jni/ffmpeg/libavutil/common.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/ctype.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/inttypes.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdint.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/limits.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/limits.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/limits.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/internal_types.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/limits.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/syslimits.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/page.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/math.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdio.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/string.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/malloc.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/alloca.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/strings.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/memory.h \
+ jni/ffmpeg/libavutil/attributes.h jni/ffmpeg/libavutil/avconfig.h \
+ jni/ffmpeg/libavutil/mem.h jni/ffmpeg/libavutil/error.h \
+ jni/ffmpeg/libavutil/mathematics.h jni/ffmpeg/libavutil/rational.h \
+ jni/ffmpeg/libavutil/intfloat_readwrite.h jni/ffmpeg/libavutil/log.h \
+ jni/ffmpeg/libavutil/pixfmt.h jni/ffmpeg/libavutil/avutil.h \
+ jni/ffmpeg/libavutil/cpu.h jni/ffmpeg/libavutil/dict.h \
+ jni/ffmpeg/libavutil/log.h jni/ffmpeg/libavutil/pixfmt.h \
+ jni/ffmpeg/libavutil/rational.h jni/ffmpeg/libavcodec/version.h \
+ jni/ffmpeg/libavutil/audioconvert.h jni/ffmpeg/libavformat/avformat.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/time.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/time.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/time.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/siginfo.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/siginfo.h \
+ jni/ffmpeg/libavformat/avio.h jni/ffmpeg/libavutil/common.h \
+ jni/ffmpeg/libavformat/version.h jni/ffmpeg/libswscale/swscale.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/jni.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/pthread.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/signal.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/signal.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/signal.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/sigcontext.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sched.h \
+ /Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/log.h
+
+jni/ffmpeg/libavcodec/avcodec.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/errno.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/errno.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/errno.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/errno.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/errno-base.h:
+
+jni/ffmpeg/libavutil/samplefmt.h:
+
+jni/ffmpeg/libavutil/avutil.h:
+
+jni/ffmpeg/libavutil/common.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/ctype.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/inttypes.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/limits.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/limits.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/limits.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/internal_types.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/limits.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/syslimits.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/page.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/math.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdio.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdlib.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/string.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/malloc.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/alloca.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/strings.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/memory.h:
+
+jni/ffmpeg/libavutil/attributes.h:
+
+jni/ffmpeg/libavutil/avconfig.h:
+
+jni/ffmpeg/libavutil/mem.h:
+
+jni/ffmpeg/libavutil/error.h:
+
+jni/ffmpeg/libavutil/mathematics.h:
+
+jni/ffmpeg/libavutil/rational.h:
+
+jni/ffmpeg/libavutil/intfloat_readwrite.h:
+
+jni/ffmpeg/libavutil/log.h:
+
+jni/ffmpeg/libavutil/pixfmt.h:
+
+jni/ffmpeg/libavutil/avutil.h:
+
+jni/ffmpeg/libavutil/cpu.h:
+
+jni/ffmpeg/libavutil/dict.h:
+
+jni/ffmpeg/libavutil/log.h:
+
+jni/ffmpeg/libavutil/pixfmt.h:
+
+jni/ffmpeg/libavutil/rational.h:
+
+jni/ffmpeg/libavcodec/version.h:
+
+jni/ffmpeg/libavutil/audioconvert.h:
+
+jni/ffmpeg/libavformat/avformat.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/time.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/time.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/time.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/siginfo.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/siginfo.h:
+
+jni/ffmpeg/libavformat/avio.h:
+
+jni/ffmpeg/libavutil/common.h:
+
+jni/ffmpeg/libavformat/version.h:
+
+jni/ffmpeg/libswscale/swscale.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/jni.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/pthread.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/signal.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/signal.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm-generic/signal.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/sigcontext.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sched.h:
+
+/Apache/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/log.h:
